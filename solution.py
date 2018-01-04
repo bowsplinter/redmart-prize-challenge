441f1a84552a50161a73a8e392fda4abfc8ca262
Vivek Kalyan <vivekkalyansk@gmail.com>
2018-01-04 15:11:04 +0800

    Add func items from csv and remove those that dont fit

--- solution.py
+++ solution.py
@@ -1,6 +1,24 @@
 import csv
 
+def get_volume(dims):
+    return reduce(lambda x,y: x*y, dims, 1)
+
+tote_dims = [45,30,35]
+tote_dims.sort()
+tote_volume = get_volume(tote_dims)
+
+items = [] #[id, price, volume, weight]
+rejected = 0
+# Get items from csv and remove those that dont fit in tote
 with open('products.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
-        print(row) 
+        item = [int(x) for x in row]
+        item_dims = item[2:5] 
+        item_dims.sort()
+        fit = (item_dims[0] <= tote_dims[0] and item_dims[1] <= tote_dims[1] and item_dims[2] <= tote_dims[2])
+        if fit:
+            items.append([item[0], item[1], get_volume(item_dims), item[5]])
+        else:
+            rejected +=1
+    print("Number of items that do not fit the tote: {}".format(rejected))
